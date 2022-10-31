5351f98dc79e1da601942372c1b661e932969849
mariaaoprea <103122570+mariaaoprea@users.noreply.github.com>
2022-10-31 13:33:25 +0100

    Update kittyconcert.py

--- 1_homework_group_22/kittyconcert.py
+++ 1_homework_group_22/kittyconcert.py
@@ -5,29 +5,10 @@ contains a constructor to name different cats
 contains a method to enable different cats to greet each other
 """
 
-class Cat:
-    """
-    class of a cat
-    """
-
-    def __init__(self, name):
-        """
-        init function for class (constructor)
-        also names the cat during initialization
-        """
-
-        self.name = name
-
-
-def greet(cat1, cat2):
-    """
-    """
-    text = f"""Hello I am {cat1.name}! I see you are also a cool fluffy kitty {cat2.name}, let’s together purr at the human, 
-            so that they shall give us food"""
-
-    print(text)
+from cat import Cat
 
 tom = Cat("tom")
 melvin = Cat("melvin")
 
-greet(tom, melvin)
+tom.greet(melvin)
+melvin.greet(tom)
